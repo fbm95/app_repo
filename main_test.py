d8d77a6b73d567d50fd6501e55a96e85fce2ef31
Bogdan Fusa <fbm_95@yahoo.com>
2021-08-14 17:13:11 +0300

    added commented salutations test

--- main_test.py
+++ main_test.py
@@ -12,5 +12,12 @@ def client():
 def test_greetings(client):
     r = client.get("/greetings")
 
-    assert r.data.decode() == "HHello!"
+    assert r.data.decode() == "Hello!"
     assert r.status_code == 200
+
+
+# def test_salutations(client):
+#     r = client.get("/greetings")
+
+#     assert r.data.decode() == "Salutations!"
+#     assert r.status_code == 200
